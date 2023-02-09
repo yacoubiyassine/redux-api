672c41e92ebb98214361dc09f7c2835d054e666b
Oumayma Yacoubi <oumayma.yacoubi@newaccess.ch>
2023-02-09 16:59:23 +0100

    msg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import React, { useState } from "react";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { add_post, delete_post, get_post, update_post } from './redux/Action/PostAction';
 
 function App() {
+  const [body,setBody]=useState("");
+  const [createdAt,setCreatedAt]=useState("");
+  const [id,setId]=useState(Math.random());
+  const [imageUrl, setImageUrl]=useState("");
+  const [namee,setNamee]=useState("");
+  const [title, setTitle]=useState("");
+  const [video, setVideo]=useState("");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch (get_post())
+  }, [dispatch])
+  const posts = useSelector((state)=>state.post);
+  console.log(posts);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="enter image url" onChange={(e)=>setImageUrl(e.target.value)}></input>
+      <button onClick={()=>dispatch(add_post({id,title,namee,imageUrl,createdAt,body,video}))}>add</button>
+      {posts.map((el)=>
+        <div>
+        <h1>{el.namee}</h1>
+        <img src={el.imageUrl} alt="404" />
+        <button onClick={()=>dispatch(delete_post(el.id))}>delete</button>
+        <button onClick={()=>dispatch(update_post(el.id,{title,namee,imageUrl,createdAt,body,video}))}>edit</button>
+        </div>
+        )}
     </div>
   );
 }
